b8f2cbe5e4ae6b7ccaca9233882052f61376e75d
DawnBreaker <tunganhngo207@gmail.com>
2025-06-25 22:08:24 +0700

    feat: update route api

---
+++ app/api/github/route.ts
@@ -0,0 +1,21 @@
+import { fetchRepoData } from "@/servers/github.server";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const params = new URL(request.url).searchParams;
+
+  const repo = params.get('repo');
+
+  if (!repo) {
+    return Response.json(
+      { message: 'Missing repo parameter' },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const data = await fetchRepoData(repo);
+
+  return Response.json(data);
+}
