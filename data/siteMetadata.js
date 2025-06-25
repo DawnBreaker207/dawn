decd627e7c9cd7449cbbcfcec61eab0703669a61
Ngo Tung Anh <126256917+DawnBreaker207@users.noreply.github.com>
2025-06-25 14:14:40 +0700

    feat: update site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,4 +1,4 @@
-/** @type {import("pliny/config").PlinyConfig } */
+// /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'Dawn Blog',
   author: 'Tung Anh',
@@ -14,7 +14,7 @@ const siteMetadata = {
   email: 'tunganhngo207@gmail.com',
   github: 'https://github.com/DawnBreaker207',
   x: 'https://twitter.com/DawnBreaker207',
-  // twitter: 'https://twitter.com/Twitter',
+  twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com/tunganh207',
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
@@ -25,13 +25,18 @@ const siteMetadata = {
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
+  socialAccounts: {
+    github: 'Karhdo',
+    linkedin: 'karhdo',
+    facebook: 'karhdo.dev',
+  },
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
     umamiAnalytics: {
       // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+      umamiWebsiteId: process.env.UMAMI_WEBSITE_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
       // You may also need to overwrite the script if you're storing data in the US - ex:
       // src: 'https://us.umami.is/script.js'
       // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
