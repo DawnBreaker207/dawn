fa865d435a7a63af4978fe2471240a40e28b55a6
DawnBreaker <tunganhngo207@gmail.com>
2025-06-25 22:05:28 +0700

    feat: update next.js config

--- next.config.js
+++ next.config.js
@@ -78,6 +78,14 @@ module.exports = () => {
           protocol: 'https',
           hostname: 'picsum.photos',
         },
+        {
+          protocol: 'https',
+          hostname: 'www.notion.so',
+        },
+        {
+          protocol: 'https',
+          hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        },
       ],
       unoptimized,
     },
