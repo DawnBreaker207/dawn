7d824b1699d2b6f0da5408705bfd64f70d8175f9
Ngo Tung Anh <126256917+DawnBreaker207@users.noreply.github.com>
2025-06-25 14:12:18 +0700

    feat: update utils

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,7 @@
-import GiscusConfigs from './giscus-config.type';
+import GiscusConfigs from './giscus-config.type'
+
 export * from './data'
 export * from './server'
 export * from './components'
 
-
-export type { GiscusConfigs}
+export type { GiscusConfigs }
--- types/server.ts
+++ types/server.ts
@@ -1,31 +1,31 @@
 export interface SpotifyNowPlayingData {
-  isPlaying: boolean;
-  songUrl?: string;
-  title?: string;
-  artist?: string;
-  album?: string;
-  albumImageUrl?: string;
+  isPlaying: boolean
+  songUrl?: string
+  title?: string
+  artist?: string
+  album?: string
+  albumImageUrl?: string
 }
 
 export interface GithubRepository {
-  stargazerCount: number;
-  description: string;
-  homepageUrl: string;
+  stargazerCount: number
+  description: string
+  homepageUrl: string
   languages: {
-    color: string;
-    name: string;
-  }[];
-  name: string;
-  nameWithOwner: string;
-  url: string;
-  forkCount: number;
-  repositoryTopics: string[];
+    color: string
+    name: string
+  }[]
+  name: string
+  nameWithOwner: string
+  url: string
+  forkCount: number
+  repositoryTopics: string[]
 }
 
 export interface ViewApiResponse {
-  total: string;
+  total: string
 }
 
 export interface TagCounts {
-  [tag: string]: number;
+  [tag: string]: number
 }
