ae0f34b5c8820d8da0051a065df5ccb6ba1d261a
DawnBreaker <tunganhngo207@gmail.com>
2025-06-25 22:07:56 +0700

    feat: update tags & project pages

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -2,10 +2,11 @@ import { slug } from 'github-slugger'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayoutWithTags'
-import { allBlogs } from 'contentlayer/generated'
 import tagData from 'app/tag-data.json'
 import { genPageMetadata } from 'app/seo'
 import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+import { allBlogs } from 'contentlayer/generated'
 
 const POSTS_PER_PAGE = 5
 
@@ -14,6 +15,7 @@ export async function generateMetadata(props: {
 }): Promise<Metadata> {
   const params = await props.params
   const tag = decodeURI(params.tag)
+
   return genPageMetadata({
     title: tag,
     description: `${siteMetadata.title} ${tag} tagged content`,
@@ -28,34 +30,30 @@ export async function generateMetadata(props: {
 
 export const generateStaticParams = async () => {
   const tagCounts = tagData as Record<string, number>
+
   const tagKeys = Object.keys(tagCounts)
-  return tagKeys.map((tag) => ({
+
+  const paths = tagKeys.map((tag) => ({
     tag: encodeURI(tag),
   }))
+
+  return paths
 }
 
 export default async function TagPage(props: { params: Promise<{ tag: string }> }) {
   const params = await props.params
   const tag = decodeURI(params.tag)
 
+  // Capitalize first letter and convert space to dash
   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
 
   const filteredPosts = allCoreContent(
     sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
   )
-  const totalPages = Math.ceil(filteredPosts.length / POSTS_PER_PAGE)
-  const initialDisplayPosts = filteredPosts.slice(0, POSTS_PER_PAGE)
-  const pagination = {
-    currentPage: 1,
-    totalPages: totalPages,
+
+  if (filteredPosts.length === 0) {
+    return notFound()
   }
 
-  return (
-    <ListLayout
-      posts={filteredPosts}
-      initialDisplayPosts={initialDisplayPosts}
-      pagination={pagination}
-      title={title}
-    />
-  )
+  return <ListLayout posts={filteredPosts} title={title} />
 }
