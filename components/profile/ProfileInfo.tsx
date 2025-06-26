82944881302a18b1b06c74b0b18cb81254245cff
DawnBreaker <tunganhngo207@gmail.com>
2025-06-26 21:35:11 +0700

    feat: update profile components

---
+++ components/profile/ProfileInfo.tsx
@@ -0,0 +1,85 @@
+import SITE_METADATA from '@/data/siteMetadata'
+import { BriefcaseBusiness, Github, Linkedin, Mail, MapPin, Twitter, X, XIcon } from 'lucide-react'
+import { Twemoji } from '../ui'
+import { Fragment } from 'react'
+function getAccountHandle(url = '') {
+  let lastPart = url.split('/').pop()
+  if (lastPart) {
+    return lastPart
+  }
+  return url
+}
+
+const SOCIALS = [
+  {
+    platform: 'github',
+    handle: getAccountHandle(SITE_METADATA.socialAccounts.github),
+    href: SITE_METADATA.github,
+    Icon: () => <Github size={20} strokeWidth={1.5} />,
+    umamiEvent: 'profile-card-github',
+  },
+  {
+    platform: 'linkedin',
+    handle: getAccountHandle(SITE_METADATA.socialAccounts.linkedin),
+    href: SITE_METADATA.linkedin,
+    Icon: () => <Linkedin size={20} strokeWidth={1.5} />,
+    umamiEvent: 'profile-card-linkedin',
+  },
+  {
+    platform: 'twitter',
+    handle: getAccountHandle(SITE_METADATA.socialAccounts.x),
+    href: SITE_METADATA.x,
+    Icon: () => <Twitter size={20} strokeWidth={1.5} />,
+    umamiEvent: 'profile-card-x',
+  },
+]
+
+export function ProfileCardInfo() {
+  return (
+    <div className="hidden py-4 md:block md:px-5">
+      <h3 className="text-xl font-semibold text-gray-800 dark:text-white">Tung Anh Ngo</h3>
+      <h5 className="py-2 text-gray-500 dark:text-gray-400">Learner | Builder</h5>
+      <div className="mt-4 mb-2 space-y-4">
+        <div className="flex items-center text-gray-700 dark:text-gray-200">
+          <BriefcaseBusiness strokeWidth={1.5} size={20} />
+          <p className="flex items-center px-2">Student</p>
+        </div>
+        <div className="flex items-center text-gray-700 dark:text-gray-200">
+          <MapPin strokeWidth={1.5} size={20} />
+          <p className="px-2">
+            Ha Noi,
+            <span className="absolute ml-1 inline-flex pt-px">
+              <Twemoji emoji="flag-vietnam" />
+            </span>
+          </p>
+        </div>
+        <div className="flex items-center text-gray-700 dark:text-gray-200">
+          <Mail strokeWidth={1.5} size={20} />
+          <a className="px-2" href={`mailto:${SITE_METADATA.email}`}>
+            {SITE_METADATA.email}
+          </a>
+        </div>
+        <div className="flex items-center gap-2.5 text-gray-700 dark:text-gray-200">
+          {SOCIALS.map(({ platform, handle, href, Icon, umamiEvent }, idx) => (
+            <Fragment key={platform}>
+              <a
+                target="_blank"
+                href={href}
+                rel="noreferrer"
+                className="flex items-center underline-offset-4 hover:underline"
+                data-umami-event={umamiEvent}
+              >
+                <Icon />
+                <span className="ml-px text-gray-500">/</span>
+                <span className="ml-0.5">{platform}</span>
+              </a>
+              {idx !== SOCIALS.length - 1 && (
+                <span className="text-gray-400 dark:text-gray-500">|</span>
+              )}
+            </Fragment>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
