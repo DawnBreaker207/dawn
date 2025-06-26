04e39d71a5a91022c08ba897231dceb6c6a01e87
DawnBreaker <tunganhngo207@gmail.com>
2025-06-26 21:35:03 +0700

    feat: update ui components

---
+++ components/ui/GrowingUnderline.tsx
@@ -0,0 +1,35 @@
+import clsx from 'clsx'
+import { CSSProperties } from 'react'
+export function GrowingUnderline({
+  as: Component = 'span',
+  children,
+  active,
+  className,
+  duration,
+  ...rest
+}: {
+  children: React.ReactNode
+  as?: React.ElementType
+  active?: boolean
+  className?: string
+  duration?: number
+} & React.ComponentProps<'span'>) {
+  return (
+    <Component
+      className={clsx([
+        'bg-linear-to-r bg-bottom-left bg-no-repeat',
+        'transition-[background-size] duration-(--duration,300ms)',
+        'from-blue-200 to-blue-100',
+        'dark:from-cyan-800 dark:to-cyan-900',
+        active
+          ? 'bg-size-[100%_50%] hover:bg-size-[100%_100%]'
+          : 'bg-size-[0px_50%] hover:bg-size-[100%_50%]',
+        className,
+      ])}
+      style={{ '--duration': `${duration || 300}ms` } as CSSProperties}
+      {...rest}
+    >
+      {children}
+    </Component>
+  )
+}
